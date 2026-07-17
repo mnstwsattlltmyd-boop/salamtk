f669d43d40bd32ebad451350ead7573140b0d797
َ <mnstwsattlltmyd@gmail.com>
2026-07-17 19:10:04 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,2 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installed');
-});
+self.addEventListener('install', e => self.skipWaiting());
+    self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
